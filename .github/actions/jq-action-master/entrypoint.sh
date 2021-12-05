493d3bf4611b40a470762f89747a6414e1cf6795
Ross Anderson <ross.anderson87@gmail.com>
2021-12-04 21:00:56 -0800

    add a patched version of gh-action-bump-version which checks the skip push flag at the right time

--- .github/actions/jq-action-master/entrypoint.sh
+++ .github/actions/jq-action-master/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 OUTPUT=$(eval $INPUT_CMD)
 
+# TODO(rosshamish): contribute this multiline support back upstream.
 if ($INPUT_MULTILINE); then
   OUTPUT="${OUTPUT//$'\n'/'%0A'}"
   OUTPUT="${OUTPUT//$'\r'/'%0D'}"
